4ded126ebcac64be18756ad81a0ed55b53a92a69
juniorolyver <76980186+juniorolyver@users.noreply.github.com>
2025-11-20 23:06:17 -0300

    feat: add page component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ import BookingItem from "./_components/booking-item";
 import { prisma } from "@/lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import Footer from "./_components/footer";
+import { PageContainer } from "./_components/ui/page";
+import { PageSectionTitle } from "./_components/ui/page";
+import { PageSection } from "./_components/ui/page";
+import { PageSectionScroller } from "./_components/ui/page";
 
 const Home = async () => {
   const recommendedBarbershops = await prisma.barbershop.findMany({
@@ -21,7 +25,7 @@ const Home = async () => {
   return (
     <main>
       <Header />
-      <div className="space-y-4 p-5">
+      <PageContainer>
         <SearchInput />
         <Image
           src={banner}
@@ -29,50 +33,36 @@ const Home = async () => {
           sizes="100vm"
           className="h-auto w-full"
         />
-        <h2 className="text-foreground text-xs font-semibold uppercase">
-          Agendamentos
-        </h2>
-        <BookingItem
-          serviceName="Corte de cabelo"
-          barbershopName="Barbearia do joão"
-          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-          date={new Date()}
-        />
+        <PageSection>
+          <PageSectionTitle>Agendamentos</PageSectionTitle>
+          <BookingItem
+            serviceName="Corte de cabelo"
+            barbershopName="Barbearia do joão"
+            barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+            date={new Date()}
+          />
+        </PageSection>
 
-        <BookingItem
-          serviceName="Corte de cabelo"
-          barbershopName="Barbearia do joão"
-          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-          date={new Date()}
-        />
+        <PageSection>
+          <PageSectionTitle>Recomendados</PageSectionTitle>
+          <PageSectionScroller>
+            {recommendedBarbershops.map((barbershop) => (
+              // <h1 key={barbershop.id}>{barbershop.name}</h1>
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSection>
 
-        <BookingItem
-          serviceName="Corte de cabelo"
-          barbershopName="Barbearia do joão"
-          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-          date={new Date()}
-        />
-
-        <h2 className="text-foreground text-xs font-semibold uppercase">
-          Recomendados
-        </h2>
-        <div className="[&:;-webkit-scrollbar]:hidden flex gap-4 overflow-x-auto">
-          {recommendedBarbershops.map((barbershop) => (
-            // <h1 key={barbershop.id}>{barbershop.name}</h1>
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
-        </div>
-
-        <h2 className="text-foreground text-xs font-semibold uppercase">
-          Populares
-        </h2>
-        <div className="[&:;-webkit-scrollbar]:hidden flex gap-4 overflow-x-auto">
-          {popularBarbershops.map((barbershop) => (
-            // <h1 key={barbershop.id}>{barbershop.name}</h1>
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
-        </div>
-      </div>
+        <PageSection>
+          <PageSectionTitle>Populares</PageSectionTitle>
+          <PageSectionScroller>
+            {popularBarbershops.map((barbershop) => (
+              // <h1 key={barbershop.id}>{barbershop.name}</h1>
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSection>
+      </PageContainer>
       <Footer />
     </main>
   );

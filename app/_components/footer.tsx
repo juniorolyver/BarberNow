fea97579ad1f53656032d672ce5137eddaafa8bd
juniorolyver <76980186+juniorolyver@users.noreply.github.com>
2025-11-20 21:52:29 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-muted flex flex-col items-center justify-center gap-1 p-7">
+      <p className="text-muted-foreground text-xs font-semibold">
+        ©:2025 BabrberNow.
+      </p>
+      <p className="text-muted-foreground text-xs">
+        Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
